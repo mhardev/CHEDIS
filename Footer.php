1a0f332e7cff4d14cf405ac3423bb2c0807642ce
Joshua Mhar Alcubilla <103411030+mhardev@users.noreply.github.com>
2024-02-14 16:30:15 +0800

    updating files

--- Footer.php
+++ Footer.php
@@ -3,6 +3,6 @@
             <p><a href="index.php">CHED Inventory System</a></p>
         </div>
         <div class="footer-copyright">
-            <p>Copyright &copy; 2024</p>
+            <p>&nbsp;Copyright &copy; 2024</p>
         </div>
 </footer>
--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <div class="login-container">
     <form action="" method="post">
-        <label for="name">Username:</label><br/>
+        <label for="name">Username:</label>
         <input type="text" name="username" id="username" placeholder="Enter username"/><br/>
-        <label for="password">Password:</label><br/>
+        <label for="password">Password:</label>
         <input type="password" name="password" id="password" placeholder="Enter password"/><br/>
     </form>
 </div>
