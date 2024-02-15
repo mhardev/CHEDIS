734a7185c2239682eadb416627cb7c2797b20879
Joshua Mhar Alcubilla <103411030+mhardev@users.noreply.github.com>
2024-02-15 11:35:12 +0800

    Refactoring files

--- Components/home.php
+++ Components/home.php
@@ -5,17 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CHED Inventory System</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
-    <link rel="stylesheet" href="Root/css/style.css">
     <link rel="stylesheet" href="Root/css/footer.css">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css"></link>
 </head>
+<?php
+    include('./Components/header.php');
+    include('./Components/sidebar.php');
+?>
 <body>
     <?php
-        include('./login.php');
+        include('./Components/body.php');
     ?>
 </body>
 <?php
-    include('./footer.php');
+    include('../footer.php');
 ?>
 <!-- JS Libraries -->
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
